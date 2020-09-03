22ea4245cda7c8e8073b5b7b33d416bbfa343dd0
Matthew Hooper <devmatthewhooper@outlook.com>
2020-09-02 20:53:55 -0500

    The Bird Feed website added to respository

---
+++ app.js
@@ -0,0 +1,5 @@
+// Bird image endpoint - https://some-random-api.ml/img/birb
+// Bird fact endpoint - https://some-random-api.ml/facts/bird
+
+let burger = document.querySelector('mobile-bar');
+let links = document.querySelector('nav-links')
